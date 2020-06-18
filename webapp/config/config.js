362f413811b8541d014bee603bf0ffa167a7038f
Darpit Chaudhary <chaudhary.da@husky.neu.edu:wq>
2020-06-17 21:17:27 -0400

    Assignment 5

--- webapp/config/config.js
+++ webapp/config/config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+module.exports = 
 {
   "development": {
     "username": "root",
     "password": "root123",
-    "database": "csye6225",
+    "database": "csye6225dum",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
@@ -16,10 +18,10 @@
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.RDS_USER_NAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DB_NAME,
+    "host": process.env.host,
     "dialect": "mysql",
     "operatorsAliases": false
   }
--- webapp/models/cart.js
+++ webapp/models/cart.js
@@ -15,7 +15,15 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.UUID,
       },
+      bookForeignId:{
+        allowNull: false,
+        type: DataTypes.UUID,
+      },
       id:{
+        allowNull: false,
+        type: DataTypes.UUID,
+      },
+      buyer_id:{
         allowNull: false,
         type: DataTypes.UUID,
       },
@@ -41,6 +49,14 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.BOOLEAN,
         defaultValue:0
       },
+    },
+    {
+      freezeTableName: true
     });
+  
+    Cart.associate = models => {
+      Cart.belongsTo(models.Users, { foreignKey: 'buyer_id'});
+      Cart.belongsTo(models.Books, { foreignKey: 'bookForeignId' });
+    };
   return Cart;
 };
